8034a4a6dfd1b902f012a9f0c5857a03987d4dbe
yoonakim <20162921@gm.hannam.ac.kr>
2022-12-03 16:50:23 +0900

    Feature: 메인 페이지 구성 완료
    김유나(22.12.03)
    
    태그: 제목
    :(space)제목 으로 :뒤에만 space를 넣는다.
    
    Feature:	새로운 기능을 추가
    Fix:	버그 수정
    Design:	CSS 등 사용자 UI 디자인 변경
    !BREAKING CHANGE:	커다란 API 변경의 경우
    !HOTFIX:	급하게 치명적인 버그를 고쳐야하는 경우
    Style:	코드 포맷 변경, 세미 콜론 누락, 코드 수정이 없는 경우
    Refactor:	프로덕션 코드 리팩토링
    Comment:	필요한 주석 추가 및 변경
    Docs:	문서 수정
    Test:	테스트 코드, 리펙토링 테스트 코드 추가, Production Code(실제로 사용하는 코드) 변경 없음
    Chore:	빌드 업무 수정, 패키지 매니저 수정, 패키지 관리자 구성 등 업데이트, Production Code 변경 없음
    Rename:	파일 혹은 폴더명을 수정하거나 옮기는 작업만인 경우
    Remove:	파일을 삭제하는 작업만 수행한 경우

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -8,6 +8,50 @@ import './Test.css';
 import { ChevronDownIcon } from '@heroicons/react/20/solid';
 
 function MainPage() {
+  const [currentButton, setCurrentButton] = useState(false);
+  const [activeButton, setActiveButton] = useState(false);
+  const [tab1, setTab1] = useState(false);
+  const [tab2, setTab2] = useState(false);
+  const [tab3, setTab3] = useState(false);
+  const [tab4, setTab4] = useState(false);
+
+  const ClickCurrentButton = () => {
+    setCurrentButton(true);
+    setActiveButton(false);
+  };
+  const ClickActiveButton = () => {
+    setCurrentButton(false);
+    setActiveButton(true);
+  };
+
+  const ClickTab1 = () => {
+    setTab1(true);
+    setTab2(false);
+    setTab3(false);
+    setTab4(false);
+  };
+
+  const ClickTab2 = () => {
+    setTab1(false);
+    setTab2(true);
+    setTab3(false);
+    setTab4(false);
+  };
+
+  const ClickTab3 = () => {
+    setTab1(false);
+    setTab2(false);
+    setTab3(true);
+    setTab4(false);
+  };
+
+  const ClickTab4 = () => {
+    setTab1(false);
+    setTab2(false);
+    setTab3(false);
+    setTab4(true);
+  };
+
   return (
     <div>
       <>
@@ -28,18 +72,142 @@ function MainPage() {
             </div>
           </header>
           <main>
-            <div className="md:grid md:grid-cols-2">
-              <div className="mx-auto w-full py-6 sm:px-6 lg:px-8">
+            <div className="md:grid md:grid-cols-2 lg:px-20">
+              <div className="mx-auto w-full py-6 sm:px-6 lg:mx-8 ">
                 {/* Replace with your content */}
                 <div className="px-4 py-6 sm:px-0">
-                  <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+                  <span className="text-2xl font-bold">Title1</span>
+
+                  <div className="more_text">
+                    <a
+                      href="#"
+                      className="focus-within:outline-none focus-within:ring-2 focus-within:ring-black focus-within:ring-offset-2 hover:text-black hover:text-pink-400"
+                    >
+                      more 〉
+                    </a>
+                  </div>
+                  <div className="md:w-full xs:w-full flex">
+                    <div className="address_button flex text-center">
+                      <button
+                        onClick={() => ClickCurrentButton()}
+                        className={
+                          currentButton === true
+                            ? 'address_button_text address_button_hover  border md:text-base text-black xs:text-sm  text-center  xs:w-full'
+                            : 'address_button_text  border md:text-base text-black xs:text-sm  text-center  xs:w-full'
+                        }
+                      >
+                        Current Tab1
+                      </button>
+                    </div>
+                    <div className=" address_button flex text-center">
+                      <button
+                        onClick={() => {
+                          ClickActiveButton();
+                        }}
+                        className={
+                          activeButton === true
+                            ? 'address_button_text address_button_hover  border md:text-base text-black xs:text-sm  text-center  xs:w-full'
+                            : 'address_button_text  border md:text-base text-black xs:text-sm  text-center  xs:w-full'
+                        }
+                      >
+                        Active Tab2
+                      </button>
+                    </div>{' '}
+                    <div className=" address_button flex text-center">
+                      <button
+                        onClick={() => setActiveButton(!activeButton)}
+                        className="address_button_text bg-gray-100 border md:text-base text-black xs:text-sm  text-center  xs:w-full "
+                      >
+                        Inactive Tab3
+                      </button>
+                    </div>
+                  </div>
+                  <div className="h-96 rounded-lg border-4 border-dashed border-gray-200">
+                    <span className="click_text">
+                      {' '}
+                      {currentButton === true ? 'Tab1 Content' : 'Tab2 Content'}
+                    </span>
+                  </div>
                 </div>
                 {/* /End replace */}
               </div>
               <div className="mx-auto w-full py-6 sm:px-6 lg:px-8">
                 {/* Replace with your content */}
                 <div className="px-4 py-6 sm:px-0">
-                  <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+                  <span className="text-2xl font-bold">Title2</span>
+
+                  <div className="more_text">
+                    <a
+                      href="#"
+                      className="focus-within:outline-none focus-within:ring-2 focus-within:ring-black focus-within:ring-offset-2 hover:text-black hover:text-pink-400"
+                    >
+                      more 〉
+                    </a>
+                  </div>
+                  <div className="md:w-full xs:w-full flex">
+                    <div className="address_button flex text-center">
+                      <button
+                        onClick={() => ClickTab1()}
+                        className={
+                          tab1 === true
+                            ? 'address_button_text address_button_hover  border md:text-base text-black xs:text-sm  text-center  xs:w-full'
+                            : 'address_button_text  border md:text-base text-black xs:text-sm  text-center  xs:w-full'
+                        }
+                      >
+                        Will check width to all tabs
+                      </button>
+                    </div>
+                    <div className="address_button flex text-center">
+                      <button
+                        onClick={() => ClickTab2()}
+                        className={
+                          tab2 === true
+                            ? 'address_button_text address_button_hover  border md:text-base text-black xs:text-sm  text-center  xs:w-full'
+                            : 'address_button_text  border md:text-base text-black xs:text-sm  text-center  xs:w-full'
+                        }
+                      >
+                        Tab2
+                      </button>
+                    </div>
+                    <div className=" address_button flex text-center">
+                      <button
+                        onClick={() => {
+                          ClickTab3();
+                        }}
+                        className={
+                          tab3 === true
+                            ? 'address_button_text address_button_hover  border md:text-base text-black xs:text-sm  text-center  xs:w-full'
+                            : 'address_button_text  border md:text-base text-black xs:text-sm  text-center  xs:w-full'
+                        }
+                      >
+                        Tab3
+                      </button>
+                    </div>{' '}
+                    <div className=" address_button flex text-center">
+                      <button
+                        onClick={() => ClickTab4()}
+                        className={
+                          tab4 === true
+                            ? 'address_button_text address_button_hover  border md:text-base text-black xs:text-sm  text-center  xs:w-full'
+                            : 'address_button_text  border md:text-base text-black xs:text-sm  text-center  xs:w-full'
+                        }
+                      >
+                        Tab4
+                      </button>
+                    </div>
+                  </div>
+                  <div className="h-96 rounded-lg border-4 border-dashed border-gray-200">
+                    <span className="click_text">
+                      {' '}
+                      {tab1 === true
+                        ? 'If width of tabs is over width of contents, that tabs will hide.'
+                        : tab2 === true
+                        ? 'Tab2 Content'
+                        : tab3 === true
+                        ? 'Tab3 Content'
+                        : 'Tab4 Content'}
+                    </span>
+                  </div>
                 </div>
                 {/* /End replace */}
               </div>

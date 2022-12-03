4be86e9aa8420aec507b3d44d35cc70bc1d500a7
yoonakim <20162921@gm.hannam.ac.kr>
2022-12-03 15:50:03 +0900

    Feature: 화면 grid 설정 1/2
    김유나(22.12.03)
    태그: 제목
    :(space)제목 으로 :뒤에만 space를 넣는다.
    
    Feature:	새로운 기능을 추가
    Fix:	버그 수정
    Design:	CSS 등 사용자 UI 디자인 변경
    !BREAKING CHANGE:	커다란 API 변경의 경우
    !HOTFIX:	급하게 치명적인 버그를 고쳐야하는 경우
    Style:	코드 포맷 변경, 세미 콜론 누락, 코드 수정이 없는 경우
    Refactor:	프로덕션 코드 리팩토링
    Comment:	필요한 주석 추가 및 변경
    Docs:	문서 수정
    Test:	테스트 코드, 리펙토링 테스트 코드 추가, Production Code(실제로 사용하는 코드) 변경 없음
    Chore:	빌드 업무 수정, 패키지 매니저 수정, 패키지 관리자 구성 등 업데이트, Production Code 변경 없음
    Rename:	파일 혹은 폴더명을 수정하거나 옮기는 작업만인 경우
    Remove:	파일을 삭제하는 작업만 수행한 경우

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -28,16 +28,15 @@ function MainPage() {
             </div>
           </header>
           <main>
-            <div className="">
-              {' '}
-              <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <div className="md:grid md:grid-cols-2">
+              <div className="mx-auto w-full py-6 sm:px-6 lg:px-8">
                 {/* Replace with your content */}
                 <div className="px-4 py-6 sm:px-0">
                   <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
                 </div>
                 {/* /End replace */}
               </div>
-              <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+              <div className="mx-auto w-full py-6 sm:px-6 lg:px-8">
                 {/* Replace with your content */}
                 <div className="px-4 py-6 sm:px-0">
                   <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
